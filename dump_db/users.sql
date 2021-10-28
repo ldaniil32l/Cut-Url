942a0b7ecefe570ac3070f62e439ca0d554330bb
ldaniil32l <popov.dan.2016@gmail.com>
2021-10-29 00:31:33 +0300

    add dump db

--- dump_db/users.sql
+++ dump_db/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 27 2021 г., 13:23
+-- Время создания: Окт 29 2021 г., 00:30
 -- Версия сервера: 5.7.33
 -- Версия PHP: 7.4.21
 
@@ -24,28 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `links`
---
-
-CREATE TABLE `links` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `long_link` varchar(250) DEFAULT NULL,
-  `short_link` varchar(20) DEFAULT NULL,
-  `views` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `links`
---
-
-INSERT INTO `links` (`id`, `user_id`, `long_link`, `short_link`, `views`) VALUES
-(1, 1, 'https://yandex.ru', 'asd', 16),
-(2, 2, 'https://google.com', 'sadsaf', 11);
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `users`
 --
 
@@ -60,20 +38,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `pass`) VALUES
-(1, 'asd', 'zsmcbasmcbasmcb'),
-(2, 'zxc', 'safagdg'),
-(3, 'dfg', 'fdgfdggdgfdgdgf');
+(5, 'qwe', '$2y$10$MyZgqXKuYz4eFJZvc4y6kOs8kXjhkSJ9RacG7iz.3996OzmGUIZTa'),
+(6, 'asd', '$2y$10$JRVp3JpdmR82U5zSiBluiOlfme0N8MIWDQ55LLcYrGzaTR08Cgtai');
 
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `links`
---
-ALTER TABLE `links`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `short_link` (`short_link`);
 
 --
 -- Индексы таблицы `users`
@@ -87,16 +57,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `links`
---
-ALTER TABLE `links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
